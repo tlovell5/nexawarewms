2575da1f2741b9a8b1d9b54f0c25fcb7b19bdc81
Your Name <you@example.com>
2023-08-12 15:04:25 -0600

    fifth commit

--- src/components/InventorySection.js
+++ src/components/InventorySection.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function InventorySection({ inventory, setInventory }) {
+
+  const initialFilters = {
+    licensePlate: "",
+    sku: "",
+    name: "",
+    lotNumber: "",
+    expirationDate: "",
+    quantity: "",
+    uom: "",
+    location: "",
+    customer: "",
+    transactionBy: "",
+    timestamp: ""
+  };
+
+  const [filters, setFilters] = useState(initialFilters);
 
   const mappingKeys = {
     "License Plate": "licensePlate",
@@ -16,6 +32,22 @@ function InventorySection({ inventory, setInventory }) {
     "Timestamp": "timestamp"
   };
 
+  const handleFilterChange = (e, key) => {
+    setFilters({
+      ...filters,
+      [key]: e.target.value
+    });
+  };
+
+  const filteredInventory = inventory.filter(product => {
+    for (let key in filters) {
+      if (filters[key] && !product[key].toString().toLowerCase().includes(filters[key].toLowerCase())) {
+        return false;
+      }
+    }
+    return true;
+  });
+
   const handleEdit = (index) => {
     const newInventory = [...inventory];
     newInventory[index].isEditing = true;
@@ -24,9 +56,7 @@ function InventorySection({ inventory, setInventory }) {
 
   const handleSave = (index) => {
     const newInventory = [...inventory];
-    
     const row = document.getElementById(`row-${index}`).querySelectorAll('td input');
-    
     Object.keys(mappingKeys).forEach((header, headerIndex) => {
       if (header !== "Timestamp") {
         newInventory[index][mappingKeys[header]] = row[headerIndex].value;
@@ -34,7 +64,6 @@ function InventorySection({ inventory, setInventory }) {
     });
 
     delete newInventory[index].isEditing;
-    
     setInventory(newInventory);
   };
 
@@ -46,29 +75,53 @@ function InventorySection({ inventory, setInventory }) {
     }
   };
 
+  const exportToCSV = () => {
+    const element = document.createElement('a');
+    const headers = Object.keys(mappingKeys);
+    const csvData = [
+      headers.join(','),
+      ...filteredInventory.map(row => headers.map(header => row[mappingKeys[header]]).join(','))
+    ].join('\n');
+
+    element.href = `data:text/csv;charset=utf-8,${encodeURIComponent(csvData)}`;
+    element.target = '_blank';
+    element.download = 'inventory.csv';
+    element.click();
+  };
+
   return (
     <section className="inventory-list" id="inventorySection">
+      <button className="export-btn" onClick={exportToCSV}>Export to CSV</button>
       <h3>Inventory List</h3>
       <table id="inventoryTable">
         <thead>
           <tr>
             {Object.keys(mappingKeys).map(header => (
-                <th key={header}>{header}</th>
+              <th key={header}>
+                {header}
+                <input 
+                  type="text"
+                  placeholder={`Filter by ${header}`}
+                  value={filters[mappingKeys[header]]}
+                  onChange={e => handleFilterChange(e, mappingKeys[header])}
+                  className="filter-input"
+                />
+              </th>
             ))}
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {inventory.map((product, index) => (
+          {filteredInventory.map((product, index) => (
             <tr key={index} id={`row-${index}`}>
               {Object.keys(mappingKeys).map(header => (
-                  <td key={header}>
-                    {product.isEditing && header !== "Timestamp" ? (
-                      <input type="text" defaultValue={product[mappingKeys[header]]} />
-                    ) : (
-                      product[mappingKeys[header]]
-                    )}
-                  </td>
+                <td key={header}>
+                  {product.isEditing && header !== "Timestamp" ? (
+                    <input type="text" defaultValue={product[mappingKeys[header]]} />
+                  ) : (
+                    product[mappingKeys[header]]
+                  )}
+                </td>
               ))}
               <td>
                 {product.isEditing ? (
